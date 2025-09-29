d914db2bfdaa6e9dd51015dde91035c5163d2763
Devin2089 <djsom525@gmail.com>
2025-09-29 15:54:29 +0000

    Receipt 3

---
+++ Receipt1.java
@@ -0,0 +1,32 @@
+/**
+ * Receipt1.java  
+ *
+ * @author – Your name
+ * @author – Class period
+ */ 
+public class Receipt1
+{
+   public static void main(String [] args)
+   {
+      String highSchoolName = "Battle Ground HS";
+      String drink = "Drink";
+      String drinkCost = "$1.50";
+      String candy = "Candy";
+      String candyCost = "$1.25";
+      String hotDog = "Hot Dog";
+      String hotDogCost = "$2.75";
+      String hamburger = "Hamburger";
+      String hamburgerCost = "$3.50";
+      System.out.println("**************************************");
+      System.out.println("*                                    *");
+      System.out.println("*    " + highSchoolName + " Snack Bar      *");
+      System.out.println("*                                    *");
+      System.out.println("*     "+drink+" .........."+drinkCost+"          *");                      
+      System.out.println("*     "+candy+" .........." +candyCost+ "          *");     
+      System.out.println("*     "+hotDog+" ........"+hotDogCost+"          *");     
+      System.out.println("*     "+hamburger+" ......"+hamburgerCost+"          *");     
+      System.out.println("*                                    *");    
+      System.out.println("**************************************");
+      System.out.prin
+   }
+}
