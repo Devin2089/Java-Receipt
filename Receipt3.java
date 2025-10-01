808a2ef14212a2df2fd6461cde4ffc0307469d89
Devin2089 <djsom525@gmail.com>
2025-10-01 16:34:59 +0000

    Checkpoint 4

--- Receipt3.java
+++ Receipt3.java
@@ -19,14 +19,31 @@ public class Receipt3
       String hamburger = "Hamburger";
       String hamburgerCost = "$3.50";
       int randomOrderNumber = (int)(Math.random() * 100) + 1;
-      int randomDrinkQuantity = (int)(Math.random() * 3);
-      int randomCandyQuantity = (int)(Math.random() * 3);
-      int randomHotdogQuantity = (int)(Math.random() * 3);
-      int randomHamburgerQuantity = (int)(Math.random() * 3);
-      Double DrinkTotal = randomDrinkQuantity * 1.50;
-      Double CandyTotal = randomCandyQuantity * 1.25;
-      Double HotdogTotal = randomHotdogQuantity * 2.75;
-      Double HamburgerTotal = randomHamburgerQuantity * 3.50;
+      
+      Scanner scan=new Scanner(System.in);
+      for (int i=0; i<1; i++) {
+          char letter = highSchoolName.charAt(i);
+          if (Character.isUpperCase(letter)) {
+              System.out.println(letter + " is uppercase");
+          } else if (Character.isLowerCase(letter)) {
+              letter = Character.toUpperCase(letter);
+          } 
+      }
+      System.out.print("Please enter High School Name: ");
+      highSchoolName = scan.nextLine();
+      System.out.print("Enter drink quantity: ");
+      int DrinkQuantity = Integer.parseInt(scan.nextLine());
+      System.out.print("Enter candy quantity: ");
+      int CandyQuantity = Integer.parseInt(scan.nextLine());
+      System.out.print("Enter hotdog quantity: ");
+      int HotdogQuantity = Integer.parseInt(scan.nextLine());
+      System.out.print("Enter hamburger quantity: ");
+      int HamburgerQuantity = Integer.parseInt(scan.nextLine());
+      scan.close();
+      Double DrinkTotal = DrinkQuantity * 1.50;
+      Double CandyTotal = CandyQuantity * 1.25;
+      Double HotdogTotal = HotdogQuantity * 2.75;
+      Double HamburgerTotal = HamburgerQuantity * 3.50;
       Double Subtotal = DrinkTotal + CandyTotal + HotdogTotal + HamburgerTotal;
       Double Tax = Subtotal * 8.6/100;
       Double TaxRounded = Math.round(Tax * 100.0) / 100.0;
@@ -46,10 +63,10 @@ public class Receipt3
       System.out.println("*                                    *");
       System.out.println("* QTY       ITEM          TOTAL      *");
       System.out.println("**************************************");
-      System.out.println("*  "+randomDrinkQuantity+"      "+drink+"          "+DrinkTotal+"         *");
-      System.out.println("*  "+randomCandyQuantity+"      "+candy+"          "+CandyTotal+"         *");
-      System.out.println("*  "+randomHotdogQuantity+"      "+hotDog+"        "+HotdogTotal+"        *");
-      System.out.println("*  "+randomHamburgerQuantity+"      "+hamburger+"      "+HamburgerTotal+"         *");
+      System.out.println("*  "+DrinkQuantity+"      "+drink+"          "+DrinkTotal+"         *");
+      System.out.println("*  "+CandyQuantity+"      "+candy+"          "+CandyTotal+"         *");
+      System.out.println("*  "+HotdogQuantity+"      "+hotDog+"        "+HotdogTotal+"        *");
+      System.out.println("*  "+HamburgerQuantity+"      "+hamburger+"      "+HamburgerTotal+"         *");
       System.out.println("**************************************");
       System.out.println("*         Subtotal:      "+Subtotal+"        *");
       System.out.println("*         Tax:           "+Tax+"        *");
